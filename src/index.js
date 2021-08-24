b577aa9a4e82b78d9c1d22389b33828ca24044e4
Alfredo Medrano <aljesamedrano@gmail.com>
2021-08-23 19:06:51 -0500

    Se inicializó el proyecto de node y se agregaron las dependencias de Express y Morgan

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+// settings
+app.set('port', process.env.PORT || 8080);
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// starting the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+})
