2bef0fb5ac40b222934d8e9ffb49c69c4861174a
Alfredo Medrano <aljesamedrano@gmail.com>
2021-08-23 19:51:09 -0500

    Se agregaron dos endpoints nuevos para consultar una lista de usuarios. y se agregó el archivo de información en formato json.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,15 @@
 const { Router } = require('express');
+const users = require('../users/users.json');
+
 router = Router();
 
+// endpoints 
 router.get('/', (req, res) => {
-    res.json({
-        'Title': 'Welcome'
-    })
+    res.send("Welcome")
+})
+
+router.get('/api/users', (req, res) => {
+    res.json(users);
 })
 
 module.exports = router;
